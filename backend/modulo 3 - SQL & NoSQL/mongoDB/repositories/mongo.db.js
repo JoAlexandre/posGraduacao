524a378870a1f3ed17c356cd4651904edde389d9
jose alexandre <jose.oliveira@lafort.com.br>
2024-06-17 11:17:31 -0400

    Backup das secrets

--- backend/modulo 3 - SQL & NoSQL/mongoDB/repositories/mongo.db.js
+++ backend/modulo 3 - SQL & NoSQL/mongoDB/repositories/mongo.db.js
@@ -2,12 +2,12 @@ import mongodb from "mongodb";
 import mongoose from "mongoose";
 //pelo driver nativo
 function getClient() {
-  const uri = `mongodb+srv://joseadoes:YYFqzKS1j3Nqlb4k@cluster0.lcrcicp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  const uri = process.env.url_db_mongo
   return new mongodb.MongoClient(uri)
 }
 
 async function connect() {
-  const uri = `mongodb+srv://joseadoes:YYFqzKS1j3Nqlb4k@cluster0.lcrcicp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  const uri = process.env.url_db_mongo
   return await mongoose.connect(uri)
 }
 
