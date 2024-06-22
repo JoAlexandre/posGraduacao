b5422aaa338605af4fd290b269701a614ceb315f
jose alexandre <jose.oliveira@lafort.com.br>
2024-06-22 08:40:10 -0400

    reset

--- backend/modulo 4 - testes/integracoes/test/server.test.js
+++ backend/modulo 4 - testes/integracoes/test/server.test.js
@@ -1,51 +1,13 @@
-const supertest = require('supertest')
-const request = supertest('http://localhost:8090')
+const supertest = require("supertest");
+const request = supertest("http://localhost:8090");
 
 describe("Testes de Integrações", () => {
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-
-  test('Servidor na porta 8090', async () => {
-    const soma = (a, b) => a + b
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-  test('Servidor na porta 8090', async () => {
-    const resp = await request.get('/')
-    expect(resp.status).toBe(200)
-  })
-  
-
-})
+	test("Servidor na porta 8090", async () => {
+		const resp = await request.get("/");
+		expect(resp.status).toBe(200);
+	});
+	test("Servidor na porta 8090", async () => {
+		const resp = await request.get("/");
+		expect(resp.status).toBe(200);
+	});
+});
